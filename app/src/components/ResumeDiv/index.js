a5fcbbd459a012d637ee51c1585d9ad933eb8b9d
Antonio Alvarado <ant.36alv@gmail.com>
2020-06-08 23:03:59 -0400

    changed google drive link

--- app/src/components/ResumeDiv/index.js
+++ app/src/components/ResumeDiv/index.js
@@ -11,7 +11,7 @@ export default function ResumeDiv() {
         <img className="pdfImage iconImage" alt="PDF" src="https://freeiconshop.com/wp-content/uploads/edd/pdf-solid.png" />
     </a>
     {/* Link to resume on google drive */}
-    <a href="https://drive.google.com/file/d/1uHEM3fGM7D5ESMbsAlJTvFBHTYLm0rc_/view?usp=sharing">
+    <a href="https://docs.google.com/document/d/1zXoLlYv1AzDE_7oIJIpmF2vPbeeY8ocA1jwnV5cHufA/edit?usp=sharing">
         <img className="iconImage driveImage" alt="Google Drive" src="http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Drive-icon.png" />
     </a>
 </div>
