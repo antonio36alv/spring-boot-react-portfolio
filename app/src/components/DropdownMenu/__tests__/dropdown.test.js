3f77d5948535d2fed9843d210914a5cf37863c1c
antonio36alv <ant.36alv@gmail.com>
2020-11-30 23:25:03 -0500

    configured testing setup, tested basic functionality of dropdown

---
+++ app/src/components/DropdownMenu/__tests__/dropdown.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import DropdownMenu from "../";
+
+describe("Dropdown menu component testing", () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<DropdownMenu/>)
+        // console.log(wrapper.debug())
+    });
+
+
+    test("render the text of dropdown menu", () => {
+        expect(wrapper.find("button").text()).toBe("Resume");
+        expect(wrapper.find("#pdf-download-link").text()).toBe("PDF Copy");
+        expect(wrapper.find("#google-drive-link").text()).toBe("Google Drive");
+    });
+
+    test("make sure nav's class changes according to clicking the dropdown menu",
+    () => {
+        expect(wrapper.find("nav").hasClass("inactive")).toBe(true)
+        wrapper.find("button").simulate("click");
+        expect(wrapper.find("nav").hasClass("active")).toBe(true)
+        wrapper.find("button").simulate("click");
+        expect(wrapper.find("nav").hasClass("inactive")).toBe(true)
+    });
+});
