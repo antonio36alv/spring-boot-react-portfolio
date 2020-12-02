f4cc18f94cb2892227defc7f0770c761cc629068
antonio36alv <ant.36alv@gmail.com>
2020-12-01 23:47:05 -0500

    cleaned up code, changed static renders to maps, also does it dynamically (i.e. won't have to make changes depending on whether solo/team projects are even or odds

--- app/src/components/Projects/index.js
+++ app/src/components/Projects/index.js
@@ -4,6 +4,26 @@ import Project from "../Project"
 import teamProjects from "./team.json"
 import soloProjects from "./solo.json"
 import "./style.css" 
+
+const renderProjects = (arr, projectsType) => {
+    return (
+        <>
+        <div className="row mb-4">
+            {arr.map((project, index) => 
+                <>
+                {index === 0 ? <h2 className="text-dark">{projectsType}</h2> : null}
+                <Project styles={index === 0 && arr.length % 2 > 0 ? "col-md-8 one-direction" : "col-md-6"}
+                    deployedLink={project.deployedLink}
+                    name={project.name} 
+                    screenshotLink={project.screenshotLink}
+                    githubLink={project.githubLink}
+                    tooltipText={project.tooltipText}/>
+                </>
+            )}
+        </div>
+        </>
+    )
+}
  
 export default function Projects() {
  
@@ -14,72 +34,13 @@ export default function Projects() {
          
      </div>
      <hr className="my-4 bg-primary" />
-
      <div id="projectsDiv">
-         
-        <div className="row mb-4">
-
-        <h2 className="text-dark">Team Projects</h2>
-
-        <Project styles="col-md-8 one-direction"
-                deployedLink={teamProjects[0].deployedLink}
-                name={teamProjects[0].name} 
-                screenshotLink={teamProjects[0].screenshotLink}
-                githubLink={teamProjects[0].githubLink}
-                tooltipText={teamProjects[0].tooltipText}/>
-        </div> 
-
-         <div className="row mb-4">
-             {/* ADD STATS */}
-            <Project styles="col-md-6"
-                deployedLink={teamProjects[1].deployedLink}
-                name={teamProjects[1].name} 
-                screenshotLink={teamProjects[1].screenshotLink}
-                githubLink={teamProjects[1].githubLink}
-                tooltipText={teamProjects[1].tooltipText}/>
-            
-            <Project styles="col-md-6"
-                deployedLink={teamProjects[2].deployedLink}
-                name={teamProjects[2].name} 
-                screenshotLink={teamProjects[2].screenshotLink}
-                githubLink={teamProjects[2].githubLink}
-                tooltipText={teamProjects[2].tooltipText}/>
-         </div> 
-         
-         <h2 className="text-dark">Solo Projects</h2>
-
-        <div className="row mb-4">
-            <Project styles="col-md-6"
-                deployedLink={soloProjects[0].deployedLink}
-                name={soloProjects[0].name} 
-                screenshotLink={soloProjects[0].screenshotLink}
-                githubLink={soloProjects[0].githubLink}
-                tooltipText={soloProjects[0].tooltipText}/>
-
-            <Project styles="col-md-6"
-                deployedLink={soloProjects[1].deployedLink}
-                name={soloProjects[1].name} 
-                screenshotLink={soloProjects[1].screenshotLink}
-                githubLink={soloProjects[1].githubLink}
-                tooltipText={soloProjects[1].tooltipText}/>
-        </div>
-
-        <div className="row mb-4">
-            <Project styles="col-md-6"
-                deployedLink={soloProjects[2].deployedLink}
-                name={soloProjects[2].name} 
-                screenshotLink={soloProjects[2].screenshotLink}
-                githubLink={soloProjects[2].githubLink}
-                tooltipText={soloProjects[2].tooltipText}/>
-
-            <Project styles="col-md-6"
-                deployedLink={soloProjects[3].deployedLink}
-                name={soloProjects[3].name} 
-                screenshotLink={soloProjects[3].screenshotLink}
-                githubLink={soloProjects[3].githubLink}
-                tooltipText={soloProjects[3].tooltipText}/>
-        </div>
-         
+        {                
+            renderProjects(teamProjects, "Team Projects")
+        }
+        {
+            renderProjects(soloProjects, "Solo Projects")
+        } 
      </div>
  </div>
 </div>
